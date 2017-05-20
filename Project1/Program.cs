a695f8a4b9a0079d9a895a713294e7fd05d20cac
Yair M <yairm@theneura.com>
2017-05-20 13:36:41 +0300

    mostly finished part 1 and 4

---
+++ Project1/Program.cs
@@ -0,0 +1,180 @@
+﻿namespace BinaryNumbers
+{
+    using System;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            binarySeries();
+        }
+
+        private static void binarySeries()
+        {
+            int[] userInputs = getInputsFromUser();
+            string[] binaryNumbers = convertToBinary(userInputs);
+            int averageNumberOfDigitsAfterBinaryConvertion = findAverageNumberOfDigits(binaryNumbers);
+            int countOfMonotonicallyIncreasingSequences = findMonotonicallyIncreasingSequences(userInputs);
+            int countOfMonotonicallyDecreasingSequences = findMonotonicallyDecreasingSequences(userInputs);
+            int averageOfInputtedNumbers = findAverageOfInputtedNumbers(userInputs);
+
+            string numbersToPrint = string.Format(
+                @"The Numbers in binary representation are: {0}, {1}, {2} ", binaryNumbers[0], binaryNumbers[1], binaryNumbers[2]);
+            string messageToPrint = string.Format(
+                @"The average number of digits in the binary representation is: {0}
+    The number of monotonically increasing sequences is: {1}.
+    The number of monotonically decreasing sequences is: {2}.
+    The average of the Values is: {3}.",
+    averageNumberOfDigitsAfterBinaryConvertion,
+    countOfMonotonicallyIncreasingSequences,
+    countOfMonotonicallyDecreasingSequences,
+    averageOfInputtedNumbers);
+
+            Console.WriteLine(numbersToPrint);
+            Console.WriteLine(messageToPrint);
+            Console.WriteLine("Please press enter to exit the program.");
+            Console.ReadLine();
+        }
+
+        private static int findAverageOfInputtedNumbers(int[] i_UserInputs)
+        {
+            int sumOfInputsValue = 0;
+            for (int i = 0; i < i_UserInputs.Length; i++)
+            {
+                sumOfInputsValue += i_UserInputs[i];
+            }
+
+            int averageOfInputtedNumbers = sumOfInputsValue / i_UserInputs.Length;
+            return averageOfInputtedNumbers;
+        }
+
+        private static int findMonotonicallyDecreasingSequences(int[] i_UserInputs)
+        {
+            int countOfMonotonicallyDecreasingSequences = 0;
+            for (int i = 0; i < i_UserInputs.Length; i++)
+            {
+                bool isAMonotonicallyDecreasingSequence = checkIfAMonotonicallyDecreasingSequence(i_UserInputs[i]);
+                if (isAMonotonicallyDecreasingSequence == true)
+                {
+                    countOfMonotonicallyDecreasingSequences++;
+                }
+            }
+
+            return countOfMonotonicallyDecreasingSequences;
+        }
+
+        private static bool checkIfAMonotonicallyDecreasingSequence(int i_PoteninalMonoticallyDecreasingSequence)
+        {
+            int unitDigit = i_PoteninalMonoticallyDecreasingSequence % 10;
+            int tensDigit = (i_PoteninalMonoticallyDecreasingSequence / 10) % 10;
+            int hundredsDigit = i_PoteninalMonoticallyDecreasingSequence / 100;
+
+            if (unitDigit < tensDigit && tensDigit < hundredsDigit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static int findMonotonicallyIncreasingSequences(int[] i_UserInputs)
+        {
+            int countOfMonotonicallyIncreasingSequences = 0;
+            for (int i = 0; i < i_UserInputs.Length; i++)
+            {
+                bool isAMonotonicallyIncreasingSequence = checkIfAMonotonicallyIncreasingSequence(i_UserInputs[i]);
+                if (isAMonotonicallyIncreasingSequence == true)
+                {
+                    countOfMonotonicallyIncreasingSequences++;
+                }
+            }
+
+            return countOfMonotonicallyIncreasingSequences;
+        }
+
+        private static bool checkIfAMonotonicallyIncreasingSequence(int i_PoteninalMonoticallyIncreasingSequence)
+        {
+            int unitDigit = i_PoteninalMonoticallyIncreasingSequence % 10;
+            int tensDigit = (i_PoteninalMonoticallyIncreasingSequence / 10) % 10;
+            int hundredsDigit = i_PoteninalMonoticallyIncreasingSequence / 100;
+
+            if (hundredsDigit < tensDigit && tensDigit < unitDigit)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static string[] convertToBinary(int[] i_UserInputs)
+        {
+            string[] binaryNumbers = new string[3];
+            int[] arrayOfInputsToWorkOn = new int[3];
+            Array.Copy(i_UserInputs, arrayOfInputsToWorkOn, 3);
+            for (int i = 0; i < arrayOfInputsToWorkOn.Length; i++)
+            {
+                StringBuilder binaryNumber = new StringBuilder();
+                while (arrayOfInputsToWorkOn[i] != 0)
+                {
+                    binaryNumber.Append(arrayOfInputsToWorkOn[i] % 2);
+                    arrayOfInputsToWorkOn[i] /= 2;
+                }
+
+                char[] reversalArray = binaryNumber.ToString().ToCharArray();
+                Array.Reverse(reversalArray);
+                binaryNumbers[i] = new string(reversalArray);
+            }
+
+            return binaryNumbers;
+        }
+
+        private static int findAverageNumberOfDigits(string[] i_BinaryNumbers)
+        {
+            int sumOfDigitsInNumbers = 0;
+
+            foreach (string binaryNumber in i_BinaryNumbers)
+            {
+                sumOfDigitsInNumbers += binaryNumber.Length;
+            }
+
+            int averageNumberOfDigits = sumOfDigitsInNumbers / i_BinaryNumbers.Length;
+
+            return averageNumberOfDigits;
+        }
+
+        private static int[] getInputsFromUser()
+        {
+            string[] arrayOfInputsFromTheUser = new string[3];
+            int[] arrayOfParsedIntegers = new int[3];
+            Console.WriteLine("Please Enter three numbers with 3 digits each.");
+
+            for (int i = 0; i < 3; i++)
+            {
+                arrayOfInputsFromTheUser[i] = Console.ReadLine();
+                if (arrayOfInputsFromTheUser[i].Length != 3)
+                {
+                    string messegeToUser = string.Format("{0} is not a valid input! Please try again.", arrayOfInputsFromTheUser[i]);
+                    Console.WriteLine(messegeToUser);
+                    i--;
+                    continue;
+                }
+
+                bool didParseWork = int.TryParse(arrayOfInputsFromTheUser[i], out arrayOfParsedIntegers[i]);
+                if (didParseWork == false || arrayOfParsedIntegers[i] < 0)
+                {
+                    string messegeToUser = string.Format("{0} is not a valid input! Please try again.", arrayOfInputsFromTheUser[i]);
+                    Console.WriteLine(messegeToUser);
+                    i--;
+                    continue;
+                }
+            }
+
+            return arrayOfParsedIntegers;
+        }
+    }
+}
---
+++ Project4/Program.cs
@@ -0,0 +1,162 @@
+﻿namespace TextAnalayzing
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            textAnalyzing();
+        }
+
+        private static void textAnalyzing()
+        {
+            string palindromeForPrint = "is not";
+            bool isStringValid = false;
+            bool isInputNumber = false;
+            string messegeToPrint = string.Empty;
+
+            Console.WriteLine("Please enter a string consisting of 8 characters that are either digits only or english characters only:");
+            string inputFromUser = getInputFromUser();
+            isStringValid = checkStringValidatiy(inputFromUser, out isInputNumber);
+
+            while (isStringValid == false)
+            {
+                Console.WriteLine("The input is invalid, try again.");
+                inputFromUser = getInputFromUser();
+                isStringValid = checkStringValidatiy(inputFromUser, out isInputNumber);
+            }
+
+            bool isStringPalindrome = checkIfStringIsPalindrome(inputFromUser);
+
+            if (isStringPalindrome == true)
+            {
+                palindromeForPrint = "is";
+            }
+
+            if (isInputNumber == true)
+            {
+                double averageOfDigits = getAverageOfDigits(inputFromUser);
+                messegeToPrint = string.Format(
+                    @"This string {0} a palindrome.
+The average value of the digits is : {1}.",
+                    palindromeForPrint,
+                    averageOfDigits);
+            }
+            else
+            {
+                int numberOfCamelCaseCharacters = getNumberOfCamelCaseCharacters(inputFromUser);
+                messegeToPrint = string.Format(
+                    @"This string {0} a palindrome.
+The number of uppercase characters is : {1}.",
+                    palindromeForPrint,
+                    numberOfCamelCaseCharacters);
+            }
+
+            Console.WriteLine(messegeToPrint);
+            Console.WriteLine("Please press enter to exit the program:");
+            Console.ReadLine();
+        }
+
+        private static int getNumberOfCamelCaseCharacters(string i_InputFromUser)
+        {
+            int numberOfCamelCaseCharacters = 0;
+            char[] characterArrayFromInput = i_InputFromUser.ToCharArray();
+
+            for (int i = 0; i < i_InputFromUser.Length; i++)
+            {
+                if (char.IsUpper(i_InputFromUser[i]) == true)
+                {
+                    numberOfCamelCaseCharacters++;
+                }
+            }
+
+            return numberOfCamelCaseCharacters;
+        }
+
+        private static double getAverageOfDigits(string i_InputFromUser)
+        {
+            double sumOfDigits = 0;
+
+            char[] characterArrayFromInput = i_InputFromUser.ToCharArray();
+
+            for (int i = 0; i < characterArrayFromInput.Length; i++)
+            {
+                sumOfDigits = sumOfDigits + (int)char.GetNumericValue(characterArrayFromInput[i]);
+            }
+
+            return sumOfDigits / characterArrayFromInput.Length;
+        }
+
+        private static bool checkIfStringIsPalindrome(string i_InputFromUser)
+        {
+            char[] characterArrayOfInputString = i_InputFromUser.ToCharArray();
+            bool isPalindrome = true;
+            for (int i = 0; i < characterArrayOfInputString.Length / 2; i++)
+            {
+                if (characterArrayOfInputString[i].Equals(characterArrayOfInputString[characterArrayOfInputString.Length - 1 - i]) == false)
+                {
+                    return !isPalindrome;
+                }
+            }
+
+            return isPalindrome;
+        }
+
+        private static bool checkStringValidatiy(string i_InputFromUser, out bool i_IsInputNumber)
+        {
+            char[] characterArrayFromInputString = i_InputFromUser.ToCharArray();
+            int numberOfDigits = 0;
+            int numberOfEnglishCharacters = 0;
+            bool stringIsValid = true;
+
+            for (int i = 0; i < characterArrayFromInputString.Length; i++)
+            {
+                if (char.IsNumber(characterArrayFromInputString[i]) == true)
+                {
+                    numberOfDigits++;
+                    continue;
+                }
+
+                bool isItAnEnglishLetter = Regex.IsMatch(characterArrayFromInputString[i].ToString(), @"[A-z]");
+                if (isItAnEnglishLetter == true)
+                {
+                    numberOfEnglishCharacters++;
+                    continue;
+                }
+            }
+
+            if (numberOfDigits != characterArrayFromInputString.Length && numberOfEnglishCharacters != characterArrayFromInputString.Length)
+            {
+                i_IsInputNumber = false;
+                return !stringIsValid;
+            }
+            else
+            {
+                if (numberOfDigits == characterArrayFromInputString.Length)
+                {
+                    i_IsInputNumber = true;
+                }
+                else
+                {
+                    i_IsInputNumber = false;
+                }
+
+                return stringIsValid;
+            }
+        }
+
+        private static string getInputFromUser()
+        {
+            string lineReadFromUser = Console.ReadLine();
+            while (lineReadFromUser.Length != 8)
+            {
+                Console.WriteLine("This input is not 8 characters long, try again.");
+                lineReadFromUser = Console.ReadLine();
+            }
+
+            return lineReadFromUser;
+        }
+    }
+}
